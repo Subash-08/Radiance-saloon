0d365d3b7fb31391e9653f68b5c11651a4d7570b
Subash-08 <128483625+Subash-08@users.noreply.github.com>
2025-04-24 23:16:49 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -110,3 +110,23 @@
     
 })(jQuery);
 
+
+// faq
+var acc = document.getElementsByClassName
+("accordion");
+var i;
+for(i = 0; i < acc.length ; i++){
+   acc[i].addEventListener("click", function(){
+    
+       this.classList.toggle("active");
+       this.parentElement.classList.toggle("active");
+
+       var pannel = this.nextElementSibling;
+
+       if(pannel.style.display === "block"){
+           pannel.style.display = "none"
+       }else {
+           pannel.style.display = "block";
+       }
+   });
+}
